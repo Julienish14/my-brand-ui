7c69136da1d4cf4e756913b036c81810b293fdd3
Juxjulien14 <juxishimwe@gmail.com>
2022-05-11 11:40:51 +0200

    update

---
+++ js/globalVariable.js
@@ -0,0 +1,20 @@
+let globalURL = window.location.origin.includes("github") ? "https://my-brand-project.herokuapp.com" : "http://localhost:4500";
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+let jwt = getCookie("jwt");
